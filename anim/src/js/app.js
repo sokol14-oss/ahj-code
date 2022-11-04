8b6797db5a184cbbc42f3c2663143f1b8e0d344c
Анастасия <natya.sokol2013@yandex.ru>
2022-11-04 17:23:13 +0500

    added

--- anim/src/js/app.js
+++ anim/src/js/app.js
@@ -1,3 +1,23 @@
-// TODO: write code here
-
-console.log('app.js bundled');
+const boarder = Array.from(document.querySelectorAll(".wrapper"));
+const person = document.querySelector(img);
+function game () {
+    const randomIndex = () => {
+        return Math.floor(Math.random() * boarder.length);
+    }
+for(let i =0; i < boarder.length; i++) {
+    const counter = boarder[randomIndex];
+    boarder[counter].classList.add(img);
+    if(boarder[counter].classList.contains(img)) {
+        boarder[counter].classList.remove(img);
+    }
+}
+const timerId = ()=> {
+    setInterval(game,500);
+setTimeout(()=> {
+    clearInterval(timerId);
+},500);
+}
+boarder.forEach(element => {
+    element.addEventListener("click",timerId);
+});
+}
